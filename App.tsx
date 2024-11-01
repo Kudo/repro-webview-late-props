c35fe579854a391a67525891fc48448b043a00e7
Kudo Chien <kudo@expo.dev>
2024-11-01 23:18:26 +0800

    add repro

--- App.tsx
+++ App.tsx
@@ -1,11 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { File, Paths } from 'expo-file-system/next';
+import { WebView } from 'react-native-webview';
+
+function createLocalHtmlFile() {
+  const html = `
+  `;
+
+  const filePath = new File(Paths.document, 'hello.html');
+  filePath.create();
+  filePath.write(`
+    <html>
+      <head>
+        <title>Local HTML File</title>
+      </head>
+      <body>
+        <h1>Hello, world!</h1>
+      </body>
+    </html>
+  `);
+  return filePath;
+}
 
 export default function App() {
+  const [htmlPath, setHtmlPath] = useState<string>('');
+  useEffect(() => {
+    const filePath = createLocalHtmlFile();
+    setHtmlPath(filePath.uri);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text>HTML file path: {htmlPath}</Text>
+      </View>
+      {htmlPath ? (
+        <WebView
+          source={{ uri: htmlPath }}
+          style={styles.webview}
+          allowFileAccess
+        />
+      ) : null}
     </View>
   );
 }
@@ -13,8 +50,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 56,
     backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  header: {
+    height: 64,
+    paddingHorizontal: 16,
+    backgroundColor: '#f0f0f0',
     justifyContent: 'center',
+  },
+  webview: {
+    flex: 1,
   },
 });
